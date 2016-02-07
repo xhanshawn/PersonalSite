288f2bf26b5d586e31f4cbf8dd70bff1534785fb
xhanshawn <xhan@wpi.edu>
2016-02-07 00:12:23 -0500

    add user and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+
+  resources :users
+
   resources :developers, param: :name
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160206161725) do
+ActiveRecord::Schema.define(version: 20160207014750) do
 
   create_table "developers", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20160206161725) do
     t.datetime "updated_at",       null: false
     t.text     "homepage_content"
     t.text     "edupage_content"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
