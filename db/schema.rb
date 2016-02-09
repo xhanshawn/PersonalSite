f5a66e8a806f3cd92c5481da12063b3def9466f1
xhanshawn <xhan@wpi.edu>
2016-02-09 11:14:46 -0500

    drop old developer table

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160208192018) do
-
-  create_table "developers", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.text     "introduction"
-  end
+ActiveRecord::Schema.define(version: 20160209161243) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
