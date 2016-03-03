db84ddaeeb8be902d9558d688a50ca4d9d2c3917
xhanshawn <xhan@wpi.edu>
2016-03-02 20:09:25 -0500

    add post

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :posts
+
   get 'homepage/index'
   root 'homepage#index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160211204259) do
+ActiveRecord::Schema.define(version: 20160303010738) do
 
   create_table "page_contents", force: :cascade do |t|
     t.integer  "user_id"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 20160211204259) do
   end
 
   add_index "page_contents", ["user_id"], name: "index_page_contents_on_user_id"
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
