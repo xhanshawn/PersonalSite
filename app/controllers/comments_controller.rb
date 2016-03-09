0ce16dd5fc80b5dc258fa31dc94643abf7e1d1cb
xhanshawn <xhan@wpi.edu>
2016-03-08 20:16:36 -0500

    complete comment create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -25,16 +25,15 @@ class CommentsController < ApplicationController
   # POST /comments.json
   def create
 
-    @post = Post.find(params[:post_id])
-
-    return if @post
+    @post = Post.find(comment_params[:post_id])
 
     @comment = @post.comments.create(comment_params)
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to @post, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
+        format.js #create.js.erb
       else
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
+
+  validates :post_id, :user_id, :body, presence: true
+
+  validates :body, uniqueness: { scope: :user_id,
+    message: "you already have an exact same comment" }
 end
