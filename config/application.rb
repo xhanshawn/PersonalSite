094e51a56c462ebd980cd6012e156927d7c4e537
xhanshawn <xhan@wpi.edu>
2016-03-13 22:12:28 -0400

    change to scss.erb

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,5 @@ module PersonalSite
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.web_console.development_only = false
-    config.serve_static_assets = true
   end
 end
