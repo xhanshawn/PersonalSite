d88175f5de501f15073a51efe9ea44be4df1d2c9
xhanshawn <xhan@wpi.edu>
2016-03-13 22:03:01 -0400

    try to

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module PersonalSite
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.web_console.development_only = false
+    config.serve_static_assets = true
   end
 end
