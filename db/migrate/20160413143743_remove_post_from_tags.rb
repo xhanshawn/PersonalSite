a998c6dccfe818f223429509c38a8f54090b4f86
xhanshawn <xhan@wpi.edu>
2016-04-15 17:45:16 -0400

    add comment for migration

--- db/migrate/20160413143743_remove_post_from_tags.rb
+++ db/migrate/20160413143743_remove_post_from_tags.rb
@@ -1,6 +1,7 @@
 class RemovePostFromTags < ActiveRecord::Migration
   def change
     remove_reference :tags, :post, index: true
-    # remove_foreign_key :tags, :posts
+    # remove_foreign_key :tags, :posts   # comment this because report error for no foreign key when do migratation
+
   end
 end
