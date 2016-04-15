d31bab62d8c9c57700570161e4f10c72d224d5c6
xhanshawn <xhan@wpi.edu>
2016-04-15 16:54:10 -0400

    change migration

--- db/migrate/20160413143743_remove_post_from_tags.rb
+++ db/migrate/20160413143743_remove_post_from_tags.rb
@@ -1,6 +1,6 @@
 class RemovePostFromTags < ActiveRecord::Migration
   def change
     remove_reference :tags, :post, index: true
-    remove_foreign_key :tags, :posts
+    # remove_foreign_key :tags, :posts
   end
 end
