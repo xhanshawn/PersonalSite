7ab3f74c2652dbe6ef1e14a068a45bf1b17c4c76
xhanshawn <xhan@wpi.edu>
2016-05-05 14:24:16 -0400

    fix the bug of validation for edge

--- app/models/edge.rb
+++ app/models/edge.rb
@@ -1,7 +1,8 @@
 class Edge < ActiveRecord::Base
 	
   belongs_to :tag
-  validates :target_id, :tag_id, inclusion: { in: Tag.ids, message: " is not found"}
+  # validates :target_id, :tag_id, inclusion: { in: Tag.ids, message: " is not found"}
+  validate :validate_tags
 
   def self.valid_edge_types
   	["parent_of", "solution_to"]
@@ -15,5 +16,10 @@ class Edge < ActiveRecord::Base
   	tag = Tag.find(self.target_id)
   end
 
-  
+  private 
+
+    def validate_tags
+      errors.add(:tag_id, " is invalid") unless Tag.exists?(self.tag_id)
+      errors.add(:target_id, " is invalid") unless Tag.exists?(self.target_id)
+    end
 end
